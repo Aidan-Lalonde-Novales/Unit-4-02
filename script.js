78fb82c43b67c28a53b6736aa11bf2b3b6bb9c29
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-07 16:05:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Function that changes text.
+
+function myFunction() {
+  document.getElementById("text").innerHTML = "Changed Text.";
+}
+
+// Function that erases text.
+
+function myFunction2() {
+  document.getElementById("text").style.display = "none";
+}
+
+// Function that changes the image.
+
+function switchOn() {
+  document.getElementById('myImage').src="images/switchon.jpg";
+}
+
+// Function that changes the image, again.
+
+function switchOff() {
+  document.getElementById('myImage').src="images/switchoff.jpg"
+}
+
+// Function that changes the background color.
+
+function changeBackground() {
+  document.getElementById('background').style.backgroundColor = "powderblue";
+}
